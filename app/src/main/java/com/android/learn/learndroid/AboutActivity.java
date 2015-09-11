a863eb5704b43fa940e7b30370d4f5e4497f1cf1
Raghav Sood <raghavsood@gmail.com>
2015-09-11 12:46:58 -0400

    How to exit the Activity

--- app/src/main/java/com/android/learn/learndroid/AboutActivity.java
+++ app/src/main/java/com/android/learn/learndroid/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.android.learn.learndroid;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,13 +12,9 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_about, menu);
-		return true;
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -28,7 +25,8 @@ public class AboutActivity extends Activity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		if (id == android.R.id.home) {
+			this.finish();
 			return true;
 		}
 
