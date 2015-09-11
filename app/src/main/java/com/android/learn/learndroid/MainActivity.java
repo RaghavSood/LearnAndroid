29c9e76c38c62b257b908826ac939526b4b9bdbf
Raghav Sood <raghavsood@gmail.com>
2015-09-11 12:41:58 -0400

    Added about activity launch

--- app/src/main/java/com/android/learn/learndroid/MainActivity.java
+++ app/src/main/java/com/android/learn/learndroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.android.learn.learndroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +30,8 @@ public class MainActivity extends Activity {
 
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_about) {
+			Intent intent = new Intent(this, AboutActivity.class);
+			startActivity(intent);
 			return true;
 		}
 
